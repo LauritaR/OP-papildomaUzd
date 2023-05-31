52c5b80b29c9241501be2397f48d1fafdca6a78e
Laurita <116920291+LauritaR@users.noreply.github.com>
2023-05-31 14:58:41 +0300

    updated fun.cpp

--- fun.cpp
+++ fun.cpp
@@ -90,7 +90,7 @@ void print(map<string, zodis>& sarasas, int eiliuSK, const string& filePath,cons
 
     for (auto key : sarasas) {
         if (key.second.cnt > 1) {
-            outputBuffer << left << setw(23) << key.first;
+            outputBuffer << left << setw(22) << key.first;
             for (int i = 0; i < key.second.eilesIndx.size(); i++) {
                 int prevIndx = (i == 0) ? 0 : key.second.eilesIndx[i - 1] + 1;
                 int lineIndx = key.second.eilesIndx[i] + 1;
@@ -111,10 +111,14 @@ void print(map<string, zodis>& sarasas, int eiliuSK, const string& filePath,cons
         }
     }
 
-    outputBuffer<<"\nURL\n";
-    for(const string& url: urls)
-    {
-        outputBuffer<<url<<endl;
+   
+    outputBuffer << "\nURL\n";
+    for (const string& url : urls) {
+        string formattedUrl = url;
+        if (formattedUrl.back()+1 == '/') {
+            formattedUrl.pop_back();  // Remove trailing slash
+        }
+        outputBuffer << formattedUrl << endl;
     }
 
     saveBuffer(filePath, outputBuffer);
@@ -126,8 +130,24 @@ void urls(vector<string>& urls, const string& filePath)
     for(string url: urls){buffer<<url<<endl;} saveBuffer(filePath,buffer);
 }
 
+bool url(const string& word) {
+    static const regex urlPattern("[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)");
+    
+    static const vector<string> excludedExtensions = {".txt", ".jpg", ".png", ".pdf", ".doc"};
+    
+    if (regex_match(word, urlPattern)) {
+    
+        for (const string& extension : excludedExtensions) {
+            if (word.length() >= extension.length() && word.substr(word.length() - extension.length()) == extension) {
+                return false;
+            }
+        }
+        return true; 
+    }
+    return false; 
+    
+}
 
-bool url(const string& word){return(regex_match(word,regex("(((http|https)://)?www\\.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)")));}
 string removeNonWord(const string& word){return regex_replace(word, regex("[-.%,?!/;:\"'()\\]\\[*0-9]"),"");}
 
 string toLower(string& word)
